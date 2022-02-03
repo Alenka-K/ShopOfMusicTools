4e839170910503abd6415ae9e39050c7c5048af5
Elena K <alenkaksa81@gmail.com>
2022-02-03 10:15:44 +0200

    Initial commit

---
+++ src/main/java/com/example/shopofmusictools/ShopOfMusicToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopofmusictools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOfMusicToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOfMusicToolsApplication.class, args);
+    }
+
+}
