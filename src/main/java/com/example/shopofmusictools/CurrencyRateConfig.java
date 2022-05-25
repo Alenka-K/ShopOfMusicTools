17b104e8d432851dffeab1b48f419e13d4e94ef1
Elena K <alenkaksa81@gmail.com>
2022-05-25 19:26:23 +0300

    #9 вынесла URL в application.properties

--- src/main/java/com/example/shopofmusictools/CurrencyRateConfig.java
+++ src/main/java/com/example/shopofmusictools/CurrencyRateConfig.java
@@ -1,16 +1,20 @@
 package com.example.shopofmusictools;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 
 
 @Configuration
+@PropertySource(value = {"classpath:application.properties"})
 public class CurrencyRateConfig {
 
-    private String url = "${currency.url}";
+    @Value("${currency.url}")
+    private String url;
 
     public URI getUrl(String ...path) {
         return UriComponentsBuilder.fromHttpUrl(url).pathSegment(path).path("/").build().toUri();
